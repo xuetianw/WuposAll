a0143790883545d7aafc23228b4059b0fbe96e13
f.mach <frances.mach@hcl.com>
2023-08-17 13:18:49 -0700

    compliance controller methods + response body

--- src/main/java/com/wupos/compliance/controller/ComplianceController.java
+++ src/main/java/com/wupos/compliance/controller/ComplianceController.java
@@ -8,9 +8,13 @@ public class ComplianceController {
     @Autowired
     private TransactionService transactionService;
 
-    @GetMapping("/sendMoney")
+    @GetMapping("/transactions")
     public ResponseEntity<String> sendMoneyValidation(@RequestBody Transaction transaction) {
-        if (transactionService.validateTransaction(transaction.getId())) {
+        if (transaction = null) {
+            return return ResponseEntity.notFound().build();
+        }
+
+        else if (transactionService.validateTransaction(transaction.getId())) {
             return ResponseEntity.badRequest().body("Invalid transaction ID");
 
         } else if (transactionService.validateMonthlyLimitAmount(transaction)) {
@@ -22,6 +26,68 @@ public class ComplianceController {
         } else {
             // success();
             return ResponseEntity.ok("Transaction successfully validated");
+        }
+    }
+
+    @GetMapping("/transactions")
+    public ResponseEntity<List<Transaction>> getAllTransactions() {
+        List<Transaction> transactions = complianceService.getAllTransactions();
+        return ResponseEntity.ok(transactions);
+    }
+
+    @GetMapping("/transactions/{id}")
+    public ResponseEntity<Transaction> getTransactionById(@PathVariable Long id) {
+        Transaction transaction = complianceService.getTransactionById(id);
+        if (transaction = null) {
+            return ResponseEntity.notFound().build();
+
+        } else {
+            return ResponseEntity.ok(transaction);
+        }
+    }
+
+    @PostMapping("/transactions")
+    public ResponseEntity<String> createTransaction(@RequestBody Transaction transaction) {
+        boolean created = complianceService.createTransaction(transaction);
+
+        if (!created) {
+            return ResponseEntity.badRequest().body("Failed to create transaction");
+
+        } else {
+            return ResponseEntity.ok("Transaction created successfully");
+        }
+    }
+
+    @PostMapping("/processTransaction")
+    public ResponseEntity<String> processTransaction(@RequestBody TransactionRequest transactionRequest) {
+        // Deserializes into the DTO
+        // ex. String firstName = transactionRequest.getCustomerEntity().getName().getFirstName();
+
+        Long id = transactionRequest.getCustomerEntity.getId();
+        return ResponseEntity.ok("Transaction processed successfully for customer ID: " + id);
+    }
+
+    @PutMapping("/transactions/{id}")
+    public ResponseEntity<String> updateTransaction(@PathVariable Long id, @RequestBody Transaction transaction) {
+        boolean updated = complianceService.updateTransaction(id, transaction);
+
+        if (!updated) {
+            return ResponseEntity.notFound().build();
+
+        } else {
+            return ResponseEntity.ok("Transaction updated successfully");
+        }
+    }
+
+    @DeleteMapping("/transactions/{id}")
+    public ResponseEntity<String> deleteTransaction(@PathVariable Long id) {
+        boolean deleted = complianceService.deleteTransaction(id);
+
+        if (!deleted) {
+            return ResponseEntity.notFound().build();
+
+        } else {
+            return ResponseEntity.ok("Transaction deleted successfully");
         }
     }
 }
--- src/main/java/com/wupos/compliance/model/Transaction.java
+++ src/main/java/com/wupos/compliance/model/Transaction.java
@@ -1,7 +1,31 @@
 package com.wupos.compliance.entity;
 
 public class Transaction {
-    CustomerEntity
-    PaymentDetailsEntity
-    ReceiverEntity
+    private CustomerEntity customerEntity;
+    private PaymentDetailsEntity paymentDetailsEntity;
+    private ReceiverEntity receiverEntity;
+
+    public CustomerEntity getCustomerEntity() {
+        return customerEntity;
+    }
+
+    public void setCustomerEntity(CustomerEntity customerEntity) {
+        this.customerEntity = customerEntity;
+    }
+
+    public PaymentDetailsEntity getPaymentDetailsEntity() {
+        return paymentDetailsEntity;
+    }
+
+    public void setPaymentDetailsEntity(PaymentDetailsEntity paymentDetailsEntity) {
+        this.paymentDetailsEntity = paymentDetailsEntity;
+    }
+
+    public ReceiverEntity getReceiverEntity() {
+        return receiverEntity;
+    }
+
+    public void setReceiverEntity(ReceiverEntity receiverEntity) {
+        this.receiverEntity = receiverEntity;
+    }
 }

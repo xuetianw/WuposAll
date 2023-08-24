c63ade7d0ee062641464422f76784f5f335ea5a8
nickpun <nickbpun@gmail.com>
2023-08-24 13:56:16 -0700

    Removed unused imports.

--- app/app/src/main/java/com/wupos/app/controller/CustomerController.java
+++ app/app/src/main/java/com/wupos/app/controller/CustomerController.java
@@ -1,15 +1,12 @@
 package com.wupos.app.controller;
 
-import com.wupos.app.model.returningParcingModel.User;
 import com.wupos.app.model.parsingModel.GetCustomerDetailsRequest;
 import com.wupos.app.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 public class CustomerController {

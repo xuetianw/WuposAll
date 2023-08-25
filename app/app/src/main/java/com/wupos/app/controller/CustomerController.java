e163ac47c4889a1ce668827b1e3d2c60f2a94175
Jacob Seol <jacob.seol@hcl.com>
2023-08-25 11:21:04 -0700

    add /addCustomer and /updateCustomer end points

--- app/app/src/main/java/com/wupos/app/controller/CustomerController.java
+++ app/app/src/main/java/com/wupos/app/controller/CustomerController.java
@@ -1,10 +1,12 @@
 package com.wupos.app.controller;
 
 import com.wupos.app.model.parsingModel.GetCustomerDetailsRequest;
+import com.wupos.app.model.returningParcingModel.User;
 import com.wupos.app.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +21,13 @@ public class CustomerController {
         return customerService.getCustomerDetails(request);
     }
 
+    @PostMapping(path = "/addCustomer")
+    public ResponseEntity<?> addCustomer(@RequestBody User request) {
+        return customerService.addCustomer(request);
+    }
+
+    @PutMapping(path = "/updateCustomer")
+    public ResponseEntity<?> updateCustomer(@RequestBody User request) {
+        return customerService.updateCustomer(request);
+    }
 }

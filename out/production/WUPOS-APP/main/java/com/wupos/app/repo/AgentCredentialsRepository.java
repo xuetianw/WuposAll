631160fce885432eb5c6c20f4564756449cb79bb
Fred <xuetian.wu@hcl.com>
2023-09-08 14:58:16 -0700

    remove out folder

--- out/production/WUPOS-APP/main/java/com/wupos/app/repo/AgentCredentialsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wupos.app.repo;
-
-import com.wupos.app.model.agent.AgentCredentials;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AgentCredentialsRepository extends JpaRepository<AgentCredentials, String> {
-}

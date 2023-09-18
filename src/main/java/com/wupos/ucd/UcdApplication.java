2d02628942c7997d37fcbfe30b432519a4bc406f
js101106 <jacob.seol@hcl.com>
2023-09-18 09:27:51 -0700

    add springbootservlerinitializer for deployment

--- src/main/java/com/wupos/ucd/UcdApplication.java
+++ src/main/java/com/wupos/ucd/UcdApplication.java
@@ -2,12 +2,19 @@ package com.wupos.ucd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UcdApplication {
+public class UcdApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UcdApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(UcdApplication.class);
 	}
 
 }

71dbbcdf231a97d2354022e98ca96a5da86aed18
Fred <xuetian.wu@hcl.com>
2023-09-19 17:17:21 -0700

    add SpringBootServletInitializer

--- src/main/java/com/example/eureka/EurekaApplication.java
+++ src/main/java/com/example/eureka/EurekaApplication.java
@@ -2,13 +2,20 @@ package com.example.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
 //@EnableDiscoveryClient
-public class EurekaApplication {
+public class EurekaApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EurekaApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
